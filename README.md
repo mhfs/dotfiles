e84e75b3688e31dccfd06c69ddf1956ad9d95563
Marcelo Silveira <marcelo@mhfs.com.br>
2012-12-30 22:29:39 -0200

    remove thor dependency

--- README.md
+++ README.md
@@ -8,36 +8,6 @@ Usage
 
 Link files:
 
-    thor dotfiles:link
-
-Vim
----
-
-I now use a separate repo for my vim stuff: https://github.com/mhfs/vimfiles
-
-Zsh
----
-
-Currently using `oh-my-zsh` with a few customizations.
-
-Rubygems
---------
-
-I don't use rdoc and ri locally.
-
-Irb
----
-
-User pry if available, otherwise Irb with some customizations:
-
-Better rails log, completion, awesome print, history, etc.
-
-Pry
----
-
-Make it work with Rails and add better prompt and output.
-
-Git
----
-
-Global gitignore and github capable gitconfig.
+```
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+dir=~/code/dotfiles         # dotfiles directory
+backup_dir=$dir/backup      # backup dir for existing dotfils
+files="ackrc gitconfig gitignore powconfig gemrc irbrc pryrc zshenv zshrc" # list of files/folders to symlink in homedir
+
+# create backup directory in dotfiles dir
+echo "Creating $backup_dir for backup of any existing dotfiles in ~"
+mkdir -p $backup_dir
+
+# cd to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+
+for file in $files; do
+  # backup existing dotfiles
+  if [ -e "$file" ]
+  then
+    echo "Moving existing dotfile from ~/.$file to $backup_dir"
+    mv ~/.$file $backup_dir/$file
+  fi
+
+  # create new links
+  echo "Creating symlink to $file in home directory."
+  ln -s $dir/$file ~/.$file
+done
+
+# linking mhfs oh-my-zsh plugin
+echo "Synlink mhfs zsh plugin to $ZSH/custom/plugins/mhfs"
+ln -s $dir/mhfs_zsh $ZSH/custom/plugins/mhfs
