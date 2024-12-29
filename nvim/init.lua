aac4c7da806896dfc3146619771f1e683dbae76c
Marcelo Silveira <mhfs@icloud.com>
2024-12-29 17:16:51 -0300

    comment

--- nvim/init.lua
+++ nvim/init.lua
@@ -1003,6 +1003,7 @@ vim.keymap.set('n', 'yrp', '<cmd>let @+=@%<CR>', { desc = '[R]elative Path' })
 vim.keymap.set('n', 'yrl', "<cmd>let @+=join([expand('%'),  line('.')], ':')<CR>", { desc = 'With [L]ine Number' })
 vim.keymap.set('n', 'yc', '<cmd>%y+<CR>', { desc = 'Buffer [C]ontent' })
 
+-- Remove trailing whitespace on save
 vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
   pattern = { '*' },
   command = [[%s/\s\+$//e]],
