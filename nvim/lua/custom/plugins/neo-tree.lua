2ab51b1197ab71ba4b0010cf7c2d80fbc13f70c7
Marcelo Silveira <mhfs@icloud.com>
2024-12-29 17:02:29 -0300

    bring back custom plugins

---
+++ nvim/lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,52 @@
+return {}
+
+-- This is the backup of what I setup for neo-tree prior to the latest kickstart upgrade.
+-- Keeping it in case I ever want to go back.
+
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+-- return {
+--   'nvim-neo-tree/neo-tree.nvim',
+--   version = '*',
+--   lazy = false, -- force load for netrw highjack to work on startup
+--   dependencies = {
+--     'nvim-lua/plenary.nvim',
+--     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+--     'MunifTanjim/nui.nvim',
+--   },
+--   cmd = 'Neotree',
+--   keys = {
+--     {
+--       '<leader>n',
+--       ':Neotree position=current reveal<CR>',
+--       desc = '[N]eoTree reveal',
+--     },
+--   },
+--   config = function()
+--     require('neo-tree').setup {
+--       enable_git_status = false,
+--       window = {
+--         position = 'current',
+--       },
+--       filesystem = {
+--         hijack_netrw_behavior = 'open_current',
+--         window = {
+--           mappings = {
+--             ['<leader>n'] = 'close_window',
+--           },
+--         },
+--       },
+--       event_handlers = {
+--         {
+--           event = 'file_opened',
+--           handler = function(_)
+--             -- auto close
+--             -- vimc.cmd("Neotree close")
+--             -- OR
+--             require('neo-tree.command').execute { action = 'close' }
+--           end,
+--         },
+--       },
+--     }
+--   end,
+-- }
