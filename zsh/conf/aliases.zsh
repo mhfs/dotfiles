fde659c0a779e211cd981786525f8f592ffb7a29
Marcelo Silveira <mhfs@icloud.com>
2025-01-02 21:21:39 -0300

    tmux theme and alias

--- zsh/conf/aliases.zsh
+++ zsh/conf/aliases.zsh
@@ -2,6 +2,7 @@
 alias ls='eza'
 # alias ls='ls --color=auto'
 alias ll='eza -l --group-directories-first --icons=always'
+alias la='eza -l -a --group-directories-first --icons=always'
 # alias ll='ls -l --color=auto'
 alias be='bundle exec'
 alias g='git'
