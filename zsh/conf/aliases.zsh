5606816aca1ea4d38fb9df4e1885351e0822637b
Marcelo Silveira <mhfs@icloud.com>
2025-11-05 08:37:58 -0300

    remove gst alias

--- zsh/conf/aliases.zsh
+++ zsh/conf/aliases.zsh
@@ -6,7 +6,6 @@ alias la='eza -l -a --group-directories-first --icons=always'
 # alias ll='ls -l --color=auto'
 alias be='bundle exec'
 alias g='git'
-alias gst='git status'
 alias ia="open $1 -a /Applications/iA\ Writer.app"
 alias reload="source $ZDOTDIR/.zshrc"
 alias cat="bat --theme='Solarized (dark)'"
