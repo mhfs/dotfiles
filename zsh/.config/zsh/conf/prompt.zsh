1e6f9def37f41e06b5b9719e52b0fc00ea73ebdd
Marcelo Silveira <mhfs@icloud.com>
2026-01-19 13:31:10 -0300

    remove pure

--- zsh/.config/zsh/conf/prompt.zsh
+++ zsh/.config/zsh/conf/prompt.zsh
@@ -1,6 +1,4 @@
-# source "$ZDOTDIR/prompts/pure.zsh"
-
 source "$ZDOTDIR/prompts/powerlevel10k/powerlevel10k.zsh-theme"
-# To customize prompt, run `p10k configure` or edit ~/.config/zsh/.p10k.zsh.
-# [[ ! -f ~/.config/zsh/prompts/p10k-powerline.zsh ]] || source ~/.config/zsh/prompts/p10k-powerline.zsh
-[[ ! -f ~/.config/zsh/prompts/p10k-lean.zsh ]] || source ~/.config/zsh/prompts/p10k-lean.zsh
+
+# To customize prompt, run `p10k configure` or edit the file below
+[[ -f ~/.config/zsh/prompts/p10k-lean.zsh ]] && source ~/.config/zsh/prompts/p10k-lean.zsh
